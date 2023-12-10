9ff816f5d6161a958de0e63ff98f6466e2517887
VladimirPetrakov <47827437+VladimirPetrakov@users.noreply.github.com>
2023-12-10 15:55:53 +0700

    Revert "Revert "fix tg bot""
    This reverts commit cc3d40edb493c27314f87194553b4e69a7df5da3.

--- telegram.php
+++ telegram.php
@@ -3,8 +3,6 @@
     require __DIR__.'/vendor/autoload.php';
     require_once  __DIR__.'/functions/facades.php';
 
-    use Bots\TelegramBot;
-
     spl_autoload_register(function ($file) {
         $file = $_SERVER['DOCUMENT_ROOT'] . '/classes/' . str_replace("\\", "/", $file) . '.php';
 
@@ -13,6 +11,9 @@
             require_once $file;
         }
     });
+
+
+    use Bots\TelegramBot;
 
     $telegramBot = new TelegramBot();
     $telegramBot->run();
